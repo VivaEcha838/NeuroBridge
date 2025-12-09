5323ed3ff4df3d2645ce6a574baf618a5b448d82
VivaEcha838 <68964649+VivaEcha838@users.noreply.github.com>
2025-12-09 02:05:56 -0800

    Refactor UserScreen to use UserProfile and add features

--- UserScreen.java
+++ UserScreen.java
@@ -1,73 +1,69 @@
 import java.util.*;
+import main.java.data.UserProfile;
+import main.java.business.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
+
 
 public class UserScreen {
     private String userName;
     private int age;
     private String primaryDiagnosis;
     private ArrayList<String> sensoryPref;
+    private ArrayList<String> communicationMethods;
+    private ArrayList<String> calmingStrategies;
+    private ArrayList<String> knownTriggers;
+    private ArrayList<String> favoriteActivities;
+    private String preferredLearning;
+    private boolean isNonVerbal;
     private String extraNotes;
+    MessageBuilder builder;
+    PhaseExtractor extractor;
+    BorderPane bp;
+    TextArea messageArea;
 
-    public UserScreen(String userName) {
-        this.userName = userName;
+    public UserScreen(UserProfile profile) {
+        this.userName = profile.userName;
         this.age = -1;
         this.primaryDiagnosis = "";
         this.sensoryPref = new ArrayList<String>();
+        this.communicationMethods = new ArrayList<String>();
+        this.calmingStrategies = new ArrayList<String>();
+        this.knownTriggers = new ArrayList<String>();
+        this.favoriteActivities = new ArrayList<String>();
+        this.preferredLearning = "";
+        this.isNonVerbal = false;
         this.extraNotes = "";
+        this.builder = new MessageBuilder();
+        this.extractor = new PhaseExtractor(builder);
+        this.bp = new BorderPane(); 
+
+        MessagePanel messagePanel = new MessagePanel(extractor);
+        bp.setCenter(messagePanel.loadTiles());
+        VBox displayBox = createDisplayBox(profile);
     }
 
-    public String getName() {
-        return this.userName;
-    }
+    public VBox createDisplayBox(UserProfile profile) {
+        VBox box = new VBox();
+        Label nameLabel = new Label("Name: " + profile.getName());
+        messageArea = new TextArea();
+        messageArea.setPrefHeight(100);
+        Button showButton = new Button("Show Message");
+        Button clearButton = new Button("Clear Message");
+        Button saveButton = new Button("Save Message");
 
-    public int getAge() {
-        return this.age;
-    }
+        // Button actions can be defined here
+        box.getChildren().addAll(nameLabel, messageArea, showButton, clearButton, saveButton);
+        return box;
 
-    public void setPrimaryDiagnosis(String primaryDiagnosis) {
-        this.primaryDiagnosis = primaryDiagnosis;
-    }
-
-    public void addSensoryPref(String preferences) {
-        sensoryPref.add(preferences);
-    }
-
-    public ArrayList<String> getSensoryPref() {
-        return this.sensoryPref;
-    }
-
-    public void setNotes(String notes) {
-        this.extraNotes = notes;
-    }
-
-    public String getNotes() {
-        return this.extraNotes;
-    }
-
-    @Override
-    public String toString() {
-        String result = "";
         
-        result += "User: " + this.userName + "\n";
-        
-        if (age > 0) {
-            result += "Age: " + this.age + "\n";
-        }
-
-        if (primaryDiagnosis != null && primaryDiagnosis.length() > 0) {
-            result += "Primary Diagnosis: " + this.primaryDiagnosis + "\n";
-        }
-        
-        if (!(sensoryPref.isEmpty())) {
-            result += "Sensory Preferences:\n";
-            for (int i = 0; i < sensoryPref.size(); i++) {
-                result += " - " + sensoryPref.get(i) + "\n";
-            }
-        }
-
-        if (extraNotes != null  && extraNotes.length() > 0) {
-            result += "Notes: " + extraNotes + "\n";
-        }
-
-        return result; 
     }
+
+     public BorderPane getScreen() {
+        return this.bp;
+    }
+
+    
 }
+
